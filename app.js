e4d9b646e0c9e85265f5a1c55edbac1317639327
Jerry Reptak <jreptak@gmail.com>
2012-11-10 11:47:31 -0500

    device orientations

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ var express = require('express')
 
 var app = express();
 
-var io = require('socket.io').listen(app);
+var server = http.createServer(app);
+var io = require('socket.io').listen(server);
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -32,14 +33,25 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 
 
+server.listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
+});
+
 io.sockets.on('connection', function (socket) {
-	socket.emit('news', { hello: 'world' });
-	socket.on('my other event', function (data) {
-		console.log(data);
+	//socket.emit('news', { hello: 'world' });
+	socket.on('motion', function (data) {
+		//var pretty = "MOTION x: " + data.x + "\t\ty: " + data.y + "\t\tz: " + data.z;
+		var pretty = data;
+		console.log(pretty);
+	});
+	socket.on('debug_data', function(data) {
+		console.dir(data);
+	});
+	socket.on('direction', function(data) {
+		console.dir(data);
+	});
+	socket.on('orientation', function(data) {
+//		var pretty = "ORIENTATION alpha: " + data.alpha + "\t\tbeta: " + data.beta + "\t\tgamma: " + data.gamma;
+	//	console.log(pretty);
 	});
 });
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
-
